bba0da1a8b018003c23553609ca24142f405ca6e
Patrick Monteith <patrick.monteith@uswitch.com>
2017-09-25 15:59:44 +0100

    Don't handle RESET action with a key on it unless it matches

--- src/routers/StackRouter.js
+++ src/routers/StackRouter.js
@@ -244,6 +244,9 @@ export default (
       }
 
       if (action.type === NavigationActions.RESET) {
+        if(action.key && action.key !== state.key) {
+          return state
+        }
         const resetAction: NavigationResetAction = action;
 
         return {
